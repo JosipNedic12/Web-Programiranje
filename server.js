e5486e5812ccbf08c10a0809d3331828e39033ea
JosipNedic12 <josip.nedic.jn@gmail.com>
2025-04-10 11:37:22 +0200

    Railway initialize

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-app.use(express.static('lv1')); // "posluzuje" index.html
-// Automatski koristi sve iz mape public
+app.use(express.static('lv1'));
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-res.send("Ili obican tekst ako nema HTML datoteke.");
+res.send('Pozdrav sa Railway servera!');
 });
-app.listen(3000, () => {
-console.log("Server pokrenut na http://localhost:3000");
+app.listen(PORT, () => {
+console.log('Server pokrenut na portu ${PORT}');
 });
